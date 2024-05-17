62fd45e6444e0e48b74737630cd55740c9782607
Madihatajammul <madihatajammul237@gmail.com>
2024-05-17 06:45:36 -0700

    update

---
+++ index.js
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+import { exit } from 'process';
+let timer = 10;
+const countDown = () => {
+    setTimeout(() => {
+        if (timer > 0) {
+            console.log(chalk.red(`Time remainig: "${timer} seconds"`));
+            timer--;
+            countDown();
+        }
+        else {
+            console.log(chalk.green('Time is up!'));
+            exit();
+        }
+    }, 1000);
+};
+const askUser = () => {
+    inquirer.prompt({
+        type: 'confirm',
+        name: 'exit',
+        message: 'Do you want to exit the timer?',
+    })
+        .then((answer) => {
+        if (answer.exit) {
+            exit();
+        }
+        else {
+            countDown();
+        }
+    });
+};
+askUser();
